20142fdc75c1efc8816002d7438dce525370ac33
Eduardo Lingan <elingan@gmail.com>
2022-07-20 13:22:31 +0200

    ajustes a la libreria

--- copy.sh
+++ copy.sh
@@ -2,4 +2,5 @@
 gulp build:lib
 # cp -R ./lib/* ../sumaq-sites-site-skeleton/node_modules/@sumaq/html-components/lib
 # cp -R ./lib/* ../../websites/www-hikeinethiopia-com/node_modules/@sumaq/html-components/lib
-cp -R ./lib/* ../../websites/www-bildraum-at/node_modules/@sumaqsites/html-components/lib
+cp -R ./lib/* ../../websites/www-bildraum-at/node_modules/@sumaq/html-components/lib
+cp -R ./lib/* ../../websites/demo-sumaqsites-com/node_modules/@sumaqsites/html-components/lib
