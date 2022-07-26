26638a70598c577ad4d6683f4786b4e913f9c374
Eduardo Lingan <elingan@gmail.com>
2022-07-26 10:32:33 +0200

    ajustes al copu

--- copy.sh
+++ copy.sh
@@ -2,5 +2,6 @@
 gulp build:lib
 # cp -R ./lib/* ../sumaq-sites-site-skeleton/node_modules/@sumaq/html-components/lib
 # cp -R ./lib/* ../../websites/www-hikeinethiopia-com/node_modules/@sumaq/html-components/lib
-cp -R ./lib/* ../../websites/www-bildraum-at/node_modules/@sumaq/html-components/lib
-cp -R ./lib/* ../../websites/demo-sumaqsites-com/node_modules/@sumaqsites/html-components/lib
+# cp -R ./lib/* ../../websites/www-bildraum-at/node_modules/@sumaq/html-components/lib
+# cp -R ./lib/* ../../websites/demo-sumaqsites-com/node_modules/@sumaqsites/html-components/lib
+cp -R ./lib/* ../../websites/www-sumaqsites-at/node_modules/@sumaqsites/html-components/lib
